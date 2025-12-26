87996d1c13f475b72b3eddae85af83f68c914897
unknown <lucas.marciano99@outlook.com>
2025-12-26 16:55:47 -0300

    Aprendendo a utilizar Class Based Views fazendo as paginas Detalhes dos Atendidos

--- atendido/views.py
+++ atendido/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from .models import Atendido
+from django.views.generic import ListView, DetailView
 
-def homepage(request):
-    atendidos = Atendido.objects.all()
-    return render(request, 'homepage.html', {'variavel_atendidos': atendidos})
+#def homepage(request):
+ #   atendidos = Atendido.objects.all()
+  #  return render(request, 'homepage.html', {'variavel_atendidos': atendidos})
+
+class Homepage(ListView):
+    model = Atendido
+    template_name = 'homepage.html'
+    #object_list é a lista de itens do modelo
+
+class DetalheAtendido(DetailView):
+    model = Atendido
+    template_name = 'detalhe_atendido.html'
+    #object é um item do modelo
