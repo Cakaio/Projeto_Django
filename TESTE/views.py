7895468b83ecc236041922e330645ce13963620a
Cakaio <caio.marciano@hotmail.com>
2025-12-29 23:22:17 -0300

    alteracoes_pagina_atendidos

--- TESTE/views.py
+++ TESTE/views.py
@@ -1,16 +1,20 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
-def homepage_inicial(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
-            login(request, user)
-            return redirect("/atendido/")  # Redirect to a named URL pattern after login
+            login(request, user)  # agora chama o login do Django corretamente
+            return redirect('inicio')  # ou o nome da sua página inicial
         else:
-            return render(request, 'homepage_inicial.html', {"error": "Usuário ou senha inválidos."})
-    return render(request, 'homepage_inicial.html')
+            return render(request, 'login.html', {'error': 'Usuário ou senha inválidos'})
+
+    return render(request, 'login.html')
+
+def inicio(request):
+    return render(request, 'inicio.html')
--- atendido/views.py
+++ atendido/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render
-from .models import Atendido
 from django.views.generic import ListView, DetailView
+from .models import Atendido
 
-#def homepage(request):
- #   atendidos = Atendido.objects.all()
-  #  return render(request, 'homepage.html', {'variavel_atendidos': atendidos})
+LISTA_SALAS = [
+    ("VIOLETA", "Violeta"),
+    ("ANIL", "Anil"),
+    ("AZUL", "Azul"),
+    ("VERDE", "Verde"),
+    ("AMARELO", "Amarelo"),
+    ("LARANJA", "Laranja"),
+    ("VERMELHO", "Vermelho"),
+    ("FAMILIA_FELIZ", "Família Feliz"),
+]
 
 class Homepage(ListView):
     model = Atendido
     template_name = 'homepage.html'
-    #object_list é a lista de itens do modelo
+    context_object_name = 'atendidos'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["salas"] = LISTA_SALAS
+        return context
+
 
 class DetalheAtendido(DetailView):
     model = Atendido
     template_name = 'detalhe_atendido.html'
-    #object é um item do modelo
