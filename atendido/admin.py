ca1df0fc1bf210765b9474eca59726294ef091a6
Cakaio <caio.marciano@hotmail.com>
2026-01-22 22:06:51 -0300

    chore: mudanças matricula de atendidos

--- atendido/admin.py
+++ atendido/admin.py
@@ -1,11 +1,35 @@
 from django.contrib import admin
-from .models import Atendido, Familia, PresencaAtendido
+from .models import Atendido, Familia, PresencaAtendido, ResponsavelAtendido
 
 # Register your models here.
-admin.site.register(Atendido)
-admin.site.register(Familia)
-
 @admin.register(PresencaAtendido)
 class PresencaAtendidoAdmin(admin.ModelAdmin):
     list_display = ['atendido', 'data', 'registrado_por']
-    list_filter = ['data']
+    list_filter = ['data', 'atendido__sala']
+
+@admin.register(Atendido)
+class AtendidoAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ("Identificação do Atendido", {'fields': ('nome', 'data_nascimento', 'rg', 'cpf', 'foto', 'documento')}),
+        ("Sala de Matricula", {'fields': ('sala',)}),
+        ("Vínculos Familiares e Responsáveis", {'fields': ('familia', 'responsavel')}),
+        ("Informaçoes Educacionais", {'fields': ('escolaridade', 'ano_escolar', 'escola','tipo_escola')}),
+        ("Contato", {'fields': ('contato',)}),
+        ("Situação Social e Atividades", {'fields': ('trabalho', 'projeto_social',)}),
+        ("Saúde e Bem-Estar", {'fields': ('convenio_medico', 'vacina_covid', 'restricao_alimentar', 'restricao_medica', 'medicacao_continua', 'deficiencia', 'comissao_inclusiva')}),
+        ("Identidade e Perfil", {'fields': ('identidade_etnica',)}),
+        ("Informações Vestuário", {'fields': ('numeracao_camisa','numeracao_calca','numeracao_calcado')}),
+        ("Termos e Autorizações", {'fields': ('termos_assinado',)}),
+        ("Observações", {'fields': ('observacoes',)}),
+        ("Controle e Auditoria do Sistema", {'fields': ('registrado_por','data_criacao','ativo')}),
+    )
+    list_display = ['nome', 'sala']
+    list_filter = ['sala']
+
+@admin.register(Familia)
+class FamiliaAdmin(admin.ModelAdmin):
+    list_display = ['nome']
+
+@admin.register(ResponsavelAtendido)
+class ResponsavelAtendidoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'parentesco']
--- atendido/models.py
+++ atendido/models.py
@@ -15,73 +15,199 @@ LISTA_SALAS = (
     ("FAMILIA_FELIZ", "Família Feliz"),
 )
 
+BAIRROS = (
+    ("NOVO HORIZONTE","Novo Horizonte"),
+    ("JARDIM NOVO HORIZONTE","Jardim Novo Horizonte"),
+    ("CECAP","CECAP"),
+    ("CECAP ALTA","CECAP Alta"),
+    ("BAIRRO DA CRUZ","Bairro da Cruz"),
+    ("CAMPOS DOS IPES","Campos dos Ipes"),
+    ("PARQUE DAS RODOVIAS","Parque das Rodovias"),
+    ("VILA INDUSTRIAL","Vila Industrial"),
+    ("VILA CRISTIANA","Vila Cristiana"),
+    ("VILA PASSOS","Vila Passos"),
+    ("VILA BRITO","Vila Brito"),
+    ("VILA DELI","Vila Deli"),
+    ("DONA ANA","Dona Ana"),
+    ("SANTA LUCRÉCIA","Santa Lucrécia"),
+    ("FAZENDA MONDEZIR","Fazenda Mondezir"),
+    ("OUTRO BAIRRO","Outro Bairro")
+)
+
+ZONAIS_RESIDENCIAIS = (
+    ("ZONA RURAL","Zona Rural"),
+    ("ZONA URBANA","Zona Urbana"),
+)
+
+SITUACAO_MORADIA = (
+    ("PRÓPRIA QUITADA","Própria Quitada"),
+    ("PRÓPRIA FINANCIADA","Própria Financiada"),
+    ("ALUGADA","Alugada"),
+    ("CEDIDA","Cedida"),
+    ("ABRIGO INSTITUCIONAL","Abrigo Institucional"),
+    ("OUTRA SITUAÇÃO","Outra Situação"),
+)
+
+CIDADES = (
+    ("LORENA","Lorena"),
+    ("PIQUETE","Piquete"),
+    ("GUARATINGUETÁ","Guaratinguetá"),
+    ("CACHOEIRA PAULISTA","Cachoeira Paulista"),
+    ("CANAS","Canas"),
+    ("CRUZEIRO","Cruzeiro"),
+    ("LAVRINHAS","Lavrinhas"),
+    ("SÃO JOSÉ DOS CAMPOS","São José dos Campos"),
+    ("TAUBATÉ","Taubaté"),
+    ("OUTRA CIDADE","Outra Cidade")
+)
+
+RENDA_FAMILIA = (
+    ("MENOS DE 1000","menos de 1000"),
+    ("ENTRE 1000-1500","Entre 1000 e 1500"),
+    ("ENTRE 1500-2000","Entre 1500 e 2000"),
+    ("ENTRE 2000-3000","Entre 2000 e 3000"),
+    ("ENTRE 3000-4000","Entre 3000 e 4000"),
+    ("ENTRE 4000-5000","Entre 4000 e 5000"),
+    ("MAIS DE 5000","Mais de 5000"),
+)
+
+ESCOLARIDADE = (
+    ("SEM ESCOLARIDADE","Sem Escolaridade"),
+    ("ENSINO FUNDAMENTAL INCOMPLETO","Ensino Fundamental Incompleto"),
+    ("ENSINO FUNDAMENTAL COMPLETO","Ensino Fundamental Completo"),
+    ("ENSINO MÉDIO INCOMPLETO","Ensino Médio Incompleto"),
+    ("ENSINO MÉDIO COMPLETO","Ensino Médio Completo"),
+    ("ENSINO SUPERIOR INCOMPLETO","Ensino Superior Incompleto"),
+    ("ENSINO SUPERIOR COMPLETO","Ensino Superior Completo"),
+    ("PÓS-GRADUAÇÃO","Pós-Graduação"),
+)
+
+ANO_ESCOLAR = (
+    ("1º ANO","1º Ano"),
+    ("2º ANO","2º Ano"),
+    ("3º ANO","3º Ano"),
+    ("4º ANO","4º Ano"),
+    ("5º ANO","5º Ano"),
+    ("6º ANO","6º Ano"),
+    ("7º ANO","7º Ano"),
+    ("8º ANO","8º Ano"),
+    ("9º ANO","9º Ano"),
+    ("1ª SÉRIE DO ENSINO MÉDIO","1ª Série do Ensino Médio"),
+    ("2ª SÉRIE DO ENSINO MÉDIO","2ª Série do Ensino Médio"),
+    ("3ª SÉRIE DO ENSINO MÉDIO","3ª Série do Ensino Médio"),
+)
+
+GRAU_PARENTESCO = (
+    ("PAI","Pai"),
+    ("MÃE","Mãe"),
+    ("AVÔ","Avô"),
+    ("AVÓ","Avó"),
+    ("TIO","Tio"),
+    ("TIA","Tia"),
+    ("IRMÃO","Irmão"),
+    ("IRMÃ","Irmã"),
+    ("OUTRO","Outro"),
+)
+
+IDENTIDADE_ETNICA = (
+    ("BRANCA","Branca"),
+    ("PRETA","Preta"),
+    ("PARDA","Parda"),
+    ("AMARELA","Amarela"),
+    ("INDÍGENA","Indígena"),
+    ("NÃO DECLARADA","Não Declarada"),
+)
+
+ESCOLA = (
+("ESCOLA RUI BRASIL", "Escola Rui Brasil"),
+("ESCOLA ESTADUAL ARNOLFO AZEVEDO", "Escola Estadual Arnolfo Azevedo"),
+("E.M PROFª HERMÍNIA FIGUEIRA DE AZEVEDO ALMEIDA", "E.M Profª Hermínia Figueira de Azevedo Almeida"),
+("CEMEI JÂNIO QUADROS", "CEMEI Jânio Quadros"),
+("APARECIDA GUEDES", "Aparecida Guedes"),
+("ESCOLA GABRIEL PRESTES", "Escola Gabriel Prestes"),
+("E.M PROFESSORA LEDA MARIA BILARD DE CARVALHO", "E.M Professora Leda Maria Bilard de Carvalho"),
+("E.E PROF. LUIZ DE CASTRO PINTO", "E.E Prof. Luiz de Castro Pinto"),
+("E.E PROF. FRANCISCO MARQUES DE OLIVEIRA JUNIOR", "E.E Prof. Francisco Marques de Oliveira Junior"),
+("EE PEI PROF.ª MIQUELINA CARTOLANO", "EE PEI Prof.ª Miquelina Cartolano"),
+("ETEC PADRE CARLOS LEÔNCIO DA SILVA", "Etec Padre Carlos Leôncio da Silva"),
+("ESCOLA MARIA AUXILIADORA", "Escola Maria Auxiliadora"),
+("ESCOLA ESTADUAL LEONOR GUIMARÃES", "Escola Estadual Leonor Guimarães"),
+("OUTRO", "Outro"),
+)
+
 class Atendido(models.Model):
-    familia = models.ForeignKey("Familia", on_delete=models.SET_NULL, null=True, blank=True, related_name="atendidos")
-    responsavel = models.ManyToManyField("ResponsavelAtendido", blank=True, related_name="atendidos")
-    nome = models.CharField(max_length=50)
-    data_nascimento = models.DateField()
-    sala = models.CharField(max_length=20, choices=LISTA_SALAS)
-    pai = models.CharField(max_length=50, blank=True, null=True)
-    mae = models.CharField(max_length=50, blank=True, null=True)
-    rg = models.CharField(max_length=15, blank=True, null=True)
-    cpf = models.CharField(max_length=14, unique=True, blank=True, null=True)
-    contato = models.CharField(max_length=20, blank=True, null=True)
-    escolaridade = models.CharField(max_length=100, blank=True, null=True)
-    #escola = models.CharField(max_length=100, blank=True, null=True) Tabela de escolas?
-    trabalho = models.CharField(max_length=100, blank=True, null=True)
-    foto = models.ImageField(upload_to='fotos_atendidos', blank=True, null=True)
-    projeto_social = models.CharField(max_length=100, blank=True, null=True)
-    convenio_medico = models.CharField(max_length=100, blank=True, null=True)
-    vacina_covid = models.BooleanField(default=False)
-    restricao_alimentar = models.TextField(blank=True, null=True)
-    restricao_medica = models.TextField(blank=True, null=True)
-    medicacao_continua = models.TextField(blank=True, null=True)
-    identidade_etnica = models.CharField(max_length=50, blank=True, null=True)
-    numeracao_camisa = models.CharField(max_length=5, blank=True, null=True)
-    numeracao_calca = models.CharField(max_length=5, blank=True, null=True)
-    numeracao_calcado = models.CharField(max_length=5, blank=True, null=True)
-    termos_assinado = models.BooleanField(default=False)
+    familia = models.ForeignKey("Familia", on_delete=models.SET_NULL, null=True, blank=True, related_name="atendidos", help_text="Família que o atendido pertence, caso não tenha, cadastre uma nova família.")
+    responsavel = models.ManyToManyField("ResponsavelAtendido", blank=True, related_name="atendidos", help_text="Responsável(s) pelo atendido, caso não tenha, cadastre um novo responsável.")
+    nome = models.CharField(max_length=50, help_text="Nome completo do atendido")
+    data_nascimento = models.DateField(help_text="Data de nascimento do atendido")
+    sala = models.CharField(max_length=20, choices=LISTA_SALAS,help_text="Sala em que o atendido está sendo matriculado")
+    rg = models.CharField(max_length=9, blank=True, null=True,help_text="RG do atendido, somente números")
+    cpf = models.CharField(max_length=11, unique=True, blank=True, null=True,help_text="CPF do atendido, somente números")
+    contato = models.CharField(max_length=11, blank=True, null=True,help_text="Número de contato do atendido, somente números")
+    documento = models.FileField(upload_to='documentos_atendidos', blank=True, null=True,help_text="Documento do atendido (RG ou Certidão de Nascimento)")
+    escolaridade = models.CharField(max_length=100, choices=ESCOLARIDADE, blank=True, null=True,help_text="Escolaridade do atendido")
+    ano_escolar = models.CharField(max_length=50, choices=ANO_ESCOLAR, blank=True, null=True,help_text="Ano escolar do atendido")
+    escola = models.CharField(max_length=100, choices=ESCOLA, blank=True, null=True,help_text="Escola do atendido")
+    tipo_escola = models.CharField(max_length=50, choices=(("PÚBLICA","Pública"),("PRIVADA","Privada")), blank=True, null=True,help_text="Tipo de escola do atendido")
+    trabalho = models.BooleanField(default=False, help_text="O atendido trabalha?")
+    foto = models.ImageField(upload_to='fotos_atendidos', blank=True, null=True,help_text="Foto do atendido")
+    projeto_social = models.BooleanField(default=False, help_text="O atendido já participou de algum projeto social?")
+    convenio_medico = models.BooleanField(default=False, help_text="O atendido possui convênio médico?")
+    vacina_covid = models.BooleanField(default=False, help_text="O atendido tomou as vacinas contra a COVID-19?")
+    restricao_alimentar = models.TextField(blank=True, null=True,help_text="Restrições alimentares do atendido")
+    restricao_medica = models.TextField(blank=True, null=True,help_text="Restrições médicas do atendido")
+    medicacao_continua = models.TextField(blank=True, null=True,help_text="Medicação contínua do atendido")
+    deficiencia = models.TextField(blank=True, null=True,help_text="O Atendido possui alguma deficiência ou necessidade educacional especial?")
+    identidade_etnica = models.CharField(max_length=50, choices=IDENTIDADE_ETNICA, blank=True, null=True,help_text="Identidade étnica do atendido")
+    numeracao_camisa = models.CharField(max_length=5, blank=True, null=True,help_text="Numeração da camisa do atendido")
+    numeracao_calca = models.CharField(max_length=5, blank=True, null=True,help_text="Numeração da calça do atendido")
+    numeracao_calcado = models.CharField(max_length=5, blank=True, null=True,help_text="Numeração do calçado do atendido")
+    termos_assinado = models.BooleanField(default=False, help_text="Os termos de: Ciencia do EDUC ; Dados/LGPD ; Uso de Imagem ; Saúde Bucal foram assinados?")
     registrado_por = models.ForeignKey("voluntario.Voluntario",on_delete=models.SET_NULL,null=True,blank=True,related_name="atendidos_registrados")
+    comissao_inclusiva = models.BooleanField(default=False,help_text="O Atendido tem necessidade de apoio da Comissão Inclusiva?")
+    observacoes = models.TextField(blank=True, null=True)
     data_criacao = models.DateTimeField(default=timezone.now)
+    ativo = models.BooleanField(default=True, help_text="Indica que o usuário será tratado como ativo. Ao invés de excluir atendidos, desmarque isso.")
 
     
 class Familia(models.Model):
-    nome = models.CharField(max_length=50, blank=True, null=True)
-    cep = models.CharField(max_length=10, blank=True, null=True)
-    endereco = models.CharField(max_length=200, blank=True, null=True)
-    bairro = models.CharField(max_length=100, blank=True, null=True)
-    cidade = models.CharField(max_length=100, blank=True, null=True)
-    agua_encanada = models.BooleanField(default=False)
-    esgoto_encanado = models.BooleanField(default=False)
-    energia_eletrica = models.BooleanField(default=False)
-    renda_total_familia = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    pessoas_moram_familia = models.IntegerField(default=1)
-    pessoas_trabalham_familia = models.IntegerField(default=0)
-    programa_transferencia_renda = models.BooleanField(default=False)
-    internet_casa = models.BooleanField(default=False)
-    comodos_casa = models.IntegerField(default=0)
-    situacao_moradia = models.CharField(max_length=100, blank=True, null=True)
-    tv_casa = models.IntegerField(default=0)
-    banheiro_casa = models.IntegerField(default=0)
-    motos_casa = models.IntegerField(default=0)
-    carros_casa = models.IntegerField(default=0)
-    geladeira_casa = models.IntegerField(default=0)
-    freezer_casa = models.IntegerField(default=0)
-    celular_casa = models.IntegerField(default=0)
-    computador_casa = models.IntegerField(default=0)
-    cesta_natal = models.BooleanField(default=False)
+    nome = models.CharField(max_length=50, blank=True, null=True,help_text="Nome da família")
+    cep = models.CharField(max_length=10, blank=True, null=True,help_text="CEP da residência da família, somente números")
+    endereco = models.CharField(max_length=200, blank=True, null=True,help_text="Endereço da residência da família")
+    bairro = models.CharField(max_length=50, choices=BAIRROS, blank=True, null=True,help_text="Bairro da residência da família")
+    cidade = models.CharField(max_length=100, choices=CIDADES, blank=True, null=True,help_text="Cidade da residência da família")
+    zona_residencial = models.CharField(max_length=20, choices=ZONAIS_RESIDENCIAIS, blank=True, null=True,help_text="Zona residencial da residência da família")
+    situacao_moradia = models.CharField(max_length=100,choices=SITUACAO_MORADIA, blank=True, null=True,help_text="Situação da moradia da família")
+    agua_encanada = models.BooleanField(default=False, help_text="A residência da família possui água encanada?")
+    esgoto_encanado = models.BooleanField(default=False, help_text="A residência da família possui esgoto encanado?")
+    energia_eletrica = models.BooleanField(default=False, help_text="A residência da família possui energia elétrica?")
+    renda_total_familia = models.CharField(max_length=20, choices=RENDA_FAMILIA, blank=True, null=True, help_text="Renda total da família")
+    pessoas_moram_familia = models.IntegerField(default=1,help_text="Número de pessoas que moram na residência da família")
+    pessoas_trabalham_familia = models.IntegerField(default=0,help_text="Número de pessoas que trabalham na residência da família")
+    programa_transferencia_renda = models.BooleanField(default=False,help_text="A família participa de algum programa de transferência de renda?")
+    internet_casa = models.BooleanField(default=False,help_text="A residência da família possui acesso à internet?")
+    comodos_casa = models.IntegerField(default=0,help_text="Número de cômodos na residência da família")
+    tv_casa = models.IntegerField(default=0,help_text="Número de televisores na residência da família")
+    banheiro_casa = models.IntegerField(default=0,help_text="Número de banheiros na residência da família")
+    motos_casa = models.IntegerField(default=0,help_text="Número de motos na residência da família")
+    carros_casa = models.IntegerField(default=0,help_text="Número de carros na residência da família")
+    geladeira_casa = models.IntegerField(default=0,help_text="Número de geladeiras na residência da família")
+    freezer_casa = models.IntegerField(default=0,help_text="Número de freezers na residência da família")
+    celular_casa = models.IntegerField(default=0,help_text="Número de celulares na residência da família")
+    computador_casa = models.IntegerField(default=0,help_text="Número de computadores na residência da família")
+    cesta_natal = models.BooleanField(default=False,help_text="A família recebeu cesta de Natal?")
     data_criacao = models.DateTimeField(default=timezone.now)
 
 class ResponsavelAtendido(models.Model):
-    nome = models.CharField(max_length=50, blank=True, null=True)
-    cpf = models.CharField(max_length=14, unique=True, null=True, blank=True)
-    rg = models.CharField(max_length=15, blank=True, null=True)
-    parentesco = models.CharField(max_length=50, null=True, blank=True)
-    contato = models.CharField(max_length=20, blank=True, null=True)
-    outro_contato = models.CharField(max_length=20, blank=True, null=True)
-    trabalho = models.CharField(max_length=100, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
-    escolaridade = models.CharField(max_length=100, blank=True, null=True)
+    nome = models.CharField(max_length=50, blank=True, null=True, help_text="Nome do responsável pelo atendido")
+    cpf = models.CharField(max_length=14, unique=True, null=True, blank=True, help_text="CPF do responsável pelo atendido, somente números")
+    rg = models.CharField(max_length=15, blank=True, null=True, help_text="RG do responsável pelo atendido, somente números")
+    parentesco = models.CharField(max_length=50, choices=GRAU_PARENTESCO, null=True, blank=True, help_text="Grau de parentesco com o atendido")
+    contato = models.CharField(max_length=20, blank=True, null=True, help_text="Número de contato do responsável pelo atendido, somente números")
+    outro_contato = models.CharField(max_length=20, blank=True, null=True, help_text="Número de contato alternativo do responsável pelo atendido, somente números")
+    trabalho = models.CharField(max_length=100, blank=True, null=True, help_text="Local de trabalho do responsável pelo atendido")
+    email = models.EmailField(blank=True, null=True, help_text="E-mail do responsável pelo atendido")
+    escolaridade = models.CharField(max_length=100, choices=ESCOLARIDADE, blank=True, null=True, help_text="Escolaridade do responsável pelo atendido")
     data_criacao = models.DateTimeField(default=timezone.now)
 
 class PresencaAtendido(models.Model):
