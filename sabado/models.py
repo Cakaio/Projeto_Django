bb49b631248a5b0738fc0c4b4960395c55c3b7ac
Cakaio <caio.marciano@hotmail.com>
2026-03-04 22:07:25 -0300

    chore: Fechar o forms quinta 21h

--- sabado/models.py
+++ sabado/models.py
@@ -24,7 +24,7 @@ class Sabado(models.Model):
     @property
     def enquete_aberta(self):
         hoje = timezone.now().date()
-        data_fechamento = self.data - timedelta(days=2)
+        data_fechamento = self.data - timedelta(days=1)
         return hoje < data_fechamento
 
 
