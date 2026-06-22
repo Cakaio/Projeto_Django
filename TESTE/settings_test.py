71370d7863955f4d3c8248966e81fa17c02113d0
vbasilioo <vbasilio2019@gmail.com>
2026-06-22 10:33:41 -0300

    fix(tests): monkey-patch Django 4.2/Python 3.14 BaseContext.__copy__ incompatibility

--- TESTE/settings_test.py
+++ TESTE/settings_test.py
@@ -7,3 +7,14 @@ DATABASES = {
         'NAME': ':memory:',
     }
 }
+
+# Monkey-patch: Django 4.2 / Python 3.14 incompatibility in BaseContext.__copy__
+import django.template.context as _django_ctx
+
+def _patched_base_context_copy(self):
+    duplicate = self.__class__.__new__(self.__class__)
+    duplicate.__dict__.update(self.__dict__)
+    duplicate.dicts = self.dicts[:]
+    return duplicate
+
+_django_ctx.BaseContext.__copy__ = _patched_base_context_copy
